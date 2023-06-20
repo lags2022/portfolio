681db4b5af7643e44f621850c5f76b859bda72c5
lags2022 <lguzman.58erb@outlook.com>
2023-06-20 17:32:51 -0500

    prueba

---
+++ app/components/Carrusel.tsx
@@ -0,0 +1,77 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "popmotion";
+import { useState } from "react";
+
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
+
+const Carrusel = () => {
+  const [[page, direction], setPage] = useState([0, 0]);
+  const imageIndex = wrap(0, 3, page);
+
+  const paginate = (newDirection: number) => {
+    setPage([page + newDirection, newDirection]);
+  };
+  return (
+    <>
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.img
+          key={page}
+          src={`/images/modal/prueba/${imageIndex}.webp`}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
+        />
+      </AnimatePresence>
+      <div className="next" onClick={() => paginate(1)}>
+        {"‣"}
+      </div>
+      <div className="prev" onClick={() => paginate(-1)}>
+        {"‣"}
+      </div>
+    </>
+  );
+};
+
+export default Carrusel;
